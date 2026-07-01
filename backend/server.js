2630a41054208c0a47d55f81ef175b605f541ae0
HemaSreeAchutha <achuthahemasree@gmail.com>
2026-07-01 19:23:54 +0530

    Refactor database connection to use env variables
    Updated database connection to use environment variables.

--- backend/server.js
+++ backend/server.js
@@ -12,11 +12,12 @@ app.use(cors());
 app.use(express.json());
 //app.use(express.urlencoded({extended:true}));
 
-const db=mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:"Hema@1896",
-    database:"diabetes_tracker"
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
 db.connect((err)=>{
