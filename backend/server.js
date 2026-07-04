59035bfd316c02a68eb54a88d5877964c2dfa375
HemaSreeAchutha <achuthahemasree@gmail.com>
2026-07-04 10:45:40 +0530

    Use environment variable for server port

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const express=require('express');
 const cors=require('cors');
 const mysql=require('mysql2');
 const app=express();
-const PORT=5000;
+const PORT=process.env.PORT || 5000;
 const auth=require("./auth");
 const jwt=require("jsonwebtoken");
 
